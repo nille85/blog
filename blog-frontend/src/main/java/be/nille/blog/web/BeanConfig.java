b4aa0536f0528ef3e11e08c7cc8386a11a3ff982
nholvoet <niels.holvoet@gmail.com>
2017-02-01 20:58:18 +0100

    vagrant setup

--- blog-frontend/src/main/java/be/nille/blog/web/BeanConfig.java
+++ blog-frontend/src/main/java/be/nille/blog/web/BeanConfig.java
@@ -10,6 +10,7 @@ import be.nille.blog.service.CategoryService;
 import be.nille.blog.service.PostService;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
+import lombok.extern.slf4j.Slf4j;
 import org.mongodb.morphia.Datastore;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,11 +20,13 @@ import org.springframework.context.annotation.Configuration;
  * @author Niels Holvoet
  */
 @Configuration
+@Slf4j
 public class BeanConfig {
 
     @Bean
     public Datastore dataStore() {
         final String url = System.getenv("MONGO_URL");
+        log.debug("MONGO URL:" + url);
         try {
             MongoClient client = new MongoClient(
                     new MongoClientURI(url)
@@ -32,7 +35,9 @@ public class BeanConfig {
             Datastore dataStore = factory.createDataStore(client, "openid-connect");
             return dataStore;
         } catch (RuntimeException ex) {
-            throw new RuntimeException("Could not connect to Mongo Database", ex);
+            log.error(ex.getMessage());
+            throw new RuntimeException(
+                    String.format("Could not connect to Mongo Database with URL %s", url));
         }
 
     }
