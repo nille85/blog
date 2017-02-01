7186574a905557b006ffd17eb7fb5d58a68cba63
nille85 <niels.holvoet@gmail.com>
2017-02-01 15:28:31 +0100

    test ssh connection to amazon

--- shippable-scripts/amazon-deploy.sh
+++ shippable-scripts/amazon-deploy.sh
@@ -2,8 +2,7 @@
 # Amazon deploy script
 
 echo "Starting deploy script"
-
-ls -a /tmp/ssh/
+##TODO Deploy using scp
 
 ssh -i "/tmp/ssh/AmazonNille" ubuntu@ec2-54-244-108-16.us-west-2.compute.amazonaws.com "bash -s $(echo "Hello from Amazon VM")"
 
