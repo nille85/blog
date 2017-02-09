8e47eb71c9025c7f7f5295a9e95601e4cf1fa80e
nholvoet <niels.holvoet@gmail.com>
2017-02-09 20:29:42 +0100

    use reflection to reconstitute objects

--- blog-mongo-service/src/main/java/be/nille/blog/mongo/category/CategoryFactory.java
+++ blog-mongo-service/src/main/java/be/nille/blog/mongo/category/CategoryFactory.java
@@ -8,7 +8,10 @@ package be.nille.blog.mongo.category;
 import be.nille.blog.domain.category.DCategory;
 import be.nille.blog.domain.category.Category;
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.apache.commons.beanutils.BeanUtils;
 import org.bson.Document;
 import org.bson.types.ObjectId;
@@ -21,15 +24,26 @@ public class CategoryFactory {
 
     public Category create(final Document document) {
         ObjectId objectId = document.getObjectId("_id");
+        Category category = create();
+        try {
+            BeanUtils.setProperty(category, "description", document.getString("description"));
+            return new MCategory(objectId.toHexString(), category);
+        } catch (IllegalAccessException | InvocationTargetException ex) {
+            throw new RuntimeException(ex.toString(), ex);
+        }
+
+    }
+
+    private Category create() {
         try {
             Class<?> c = Class.forName(DCategory.class.getName());
             Constructor<?> constructor = c.getDeclaredConstructor();
             constructor.setAccessible(true);
-            Category category = DCategory.class.newInstance();
-            BeanUtils.setProperty(category, "description", document.get("description"));
-            return new MCategory(objectId.toHexString(), category);
-        } catch (InstantiationException | IllegalAccessException | InvocationTargetException | ClassNotFoundException | NoSuchMethodException ex) {
-            throw new RuntimeException(ex.toString());
+            Object category = constructor.newInstance();
+
+            return (Category) category;
+        } catch (IllegalArgumentException | InvocationTargetException | InstantiationException | IllegalAccessException | ClassNotFoundException | NoSuchMethodException ex) {
+            throw new RuntimeException(ex.toString(), ex);
         }
     }
 
